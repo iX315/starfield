3a5c9a3c0f4b7e851cb917adf6421b9b75b0df3f
Jury Giannelli <iX315@me.com>
2020-07-11 00:52:55 +0200

    fix index

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,3 @@
 import { Starfield } from './Starfield';
 
-console.log('See this in your browser console: Typescript Webpack Starter Launched');
-
-const container = <HTMLCanvasElement> document.getElementById('starfield_container')
-
-const starfield = new Starfield(container);
-starfield.start()
-
-window['starfield'] = starfield
+export default Starfield
