79a14f3adbc052d03f15e8b4a21ea3e143d6edb6
Jury Giannelli <iX315@me.com>
2020-10-02 23:11:38 +0200

    add dev mode for webpack

--- src/index.dev.ts
+++ src/index.dev.ts
@@ -2,6 +2,6 @@ import { Starfield } from './Starfield'
 
 export default (() => {
     const container = <HTMLCanvasElement> document.getElementById('starfield_container')
-    const starfield = new Starfield(container)
+    const starfield = new Starfield(container, { showDebug: true })
     starfield.start()
 })()
