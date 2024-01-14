9edb2a5c6d0af0b0c04e67622ba213d2be436fff
Jury Giannelli <jury.giannelli@sdox.io>
2024-01-14 15:28:26 +0100

    add react component

--- src/Starfield.component.tsx
+++ src/Starfield.component.tsx
@@ -1,21 +1,30 @@
-import type { Options } from './Options'
-import {Starfield as StarfieldClass} from './Starfield'
-import {CanvasHTMLAttributes, DetailedHTMLProps, useEffect, useRef} from 'react'
+import {useEffect, useRef} from 'react'
 
-export interface StarfieldProps extends DetailedHTMLProps<CanvasHTMLAttributes<HTMLCanvasElement>, HTMLCanvasElement> {
-    options: Options
+import {Starfield as StarfieldClass} from './Starfield'
+
+import type {Options} from './Options'
+import type {CanvasHTMLAttributes, DetailedHTMLProps} from 'react'
+
+type CanvasProps = DetailedHTMLProps<CanvasHTMLAttributes<HTMLCanvasElement>, HTMLCanvasElement>
+
+export interface StarfieldProps extends CanvasProps {
+  options: Options
 }
 
 export const Starfield = ({options, ...restProps}: StarfieldProps) => {
-    const canvasRef = useRef<HTMLCanvasElement>(null!)
-    
-    useEffect(() => {
-        if (canvasRef.current) {
-            new StarfieldClass(canvasRef.current, options)
-        }
-    }, [canvasRef])
+  const canvasRef = useRef<HTMLCanvasElement>(null!)
 
-    return (
-        <canvas ref={canvasRef} {...restProps}></canvas>
-    )
+  useEffect(() => {
+    if (canvasRef.current) {
+      new StarfieldClass(canvasRef.current, options)
+    }
+  }, [canvasRef])
+
+  return (
+    <canvas
+      ref={canvasRef}
+      {...restProps}
+    >
+    </canvas>
+  )
 }
