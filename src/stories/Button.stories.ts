6a72585a70243925fe9ff2f9f7b69db339490921
Jury Giannelli <iX315@me.com>
2025-08-02 15:52:05 +0200

    add storybook

---
+++ src/stories/Button.stories.ts
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/react-vite'
+
+import { Starfield } from '../Starfield.component'
+
+const meta = {
+  title: 'Starfield',
+  component: Starfield,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Starfield>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+
+export const Default: Story = {
+  args: {},
+}
+
+export const Fullscreen: Story = {
+  parameters: {
+    layout: 'fullscreen',
+  },
+  args: {}
+}
