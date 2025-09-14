234bd12e4b408ad7f496e6e544a1c114a95c9350
Jury Giannelli <jury.giannelli@sdox.io>
2025-09-14 13:30:23 +0200

    Updates dependencies and configures build
    Updates project dependencies, including `@types/node`, `rimraf`, and `vite-plugin-dts`.
    
    Refactors the `tsconfig.json` to `tsconfig.lib.json` for library specific configurations and updates the main `tsconfig.json` to exclude source files and include node types for vitest.
    
    Configures the build process using `vite.config.ts` and `vitest.config.ts`.

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,29 @@
 import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [
     react(),
     dts({
       insertTypesEntry: true,
-      outDir: 'types',
-    }),
+      outDir: 'types'
+    })
   ],
   build: {
     copyPublicDir: false,
     lib: {
       entry: '/src/index.ts',
       name: 'starfield-next',
-      fileName: (format) => `starfield-next.${format}.js`,
+      fileName: (format) => `starfield-next.${format}.js`
     },
     rollupOptions: {
       external: ['react'],
       output: {
         globals: {
-          react: 'React',
-        },
-      },
-    },
+          react: 'React'
+        }
+      }
+    }
   }
 })
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,10 @@
-/// <reference types="vitest/config" />
-import { defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
-const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import { fileURLToPath } from 'node:url'
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin'
+
+const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url))
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
@@ -22,9 +22,9 @@ export default defineConfig({
       plugins: [
       // The plugin will run tests for the stories defined in your Storybook config
       // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-      storybookTest({
-        configDir: path.join(dirname, '.storybook')
-      })],
+        storybookTest({
+          configDir: path.join(dirname, '.storybook')
+        })],
       test: {
         name: 'storybook',
         browser: {
@@ -39,4 +39,4 @@ export default defineConfig({
       }
     }]
   }
-});
+})
