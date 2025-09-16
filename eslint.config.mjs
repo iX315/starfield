bc1f65ca4a963361c9a9fe2177dcfcf6235483b5
Jury Giannelli <jury.giannelli@sdox.io>
2025-09-16 20:47:40 +0200

    linter fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'eslint/config'
 import eslintConfig from '@ix315/eslint-config'
 
 export default defineConfig([
-  eslintConfig.recommended,
+  eslintConfig.recommended
 ])
